881b7a62f4e718db46c83ff6dabc7307020de4a5
Andrea Lopez <alopezgo1991@gmail.com>
2022-12-07 16:39:45 -0300

    se agrega modal y botones condicionales segun user-agent

--- core/views.py
+++ core/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django_user_agents.utils import get_user_agent
+from django.http import FileResponse
+from django.http import HttpResponse
+import os
 
 # Create your views here.
+path = os.path.realpath('C:/Users/Usuario/Desktop/file.png')
 
 def home (request):
+    
     user_agent = get_user_agent(request)
     if user_agent.is_mobile:
-        file = "Hola eres un telefono móvil"
-    elif user_agent.is_pc:
-        file = "Hola eres un pc"
-    elif user_agent.is_tablet:
-        file = "Hola eres una tablet"
+        message = "Descarga la app desde tu teléfono móvil"
 
-    return render(request, 'core/home.html', {"data" : file})
+    else:
+        message = "Genera código QR para descargar la app"
+        
+    return render(request, 'core/home.html', {"data" : message})
+
+def download (path):
+    file = open(path,"rb")
+    response = FileResponse(file) 
+    response["Content-disposition"] = "attachment; filename={}".format(os.path.basename(path))
+    return response
